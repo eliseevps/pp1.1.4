42fa16b4578039ab31a8ad20fdc22d6825e77374
eliseevps <eliseev.ps@mail.ru>
2023-07-11 09:00:57 +0500

    Done connection with postgres

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,23 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private final static String URL = "jdbc:postgresql://localhost:5432/postgres";
+    private final static String USER = "postgres";
+    private final static String PASSWORD = "postgresql";
+    private static Connection connection;
+
+    public static Connection getConnection() {
+        try {
+            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            connection.setAutoCommit(false);
+            System.out.println("Connection successful!");
+        } catch (SQLException e) {
+            System.err.println("Connection failed!");
+        }
+        return connection;
+    }
 }
